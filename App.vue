44e79a33e846fefb2e1117fd144b216adab6f668
yyasat <3094576451@qq.com>
2026-06-13 22:37:47 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  onLaunch: function() {
+    console.log('App Launched')
+	// 进入全屏模式（隐藏状态栏，保留返回键）
+	plus.navigator.setFullscreen(true);
+  },
+  onShow: function() {
+    console.log('App Show')
+  },
+  onHide: function() {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style>
+/* 全局样式 */
+@import './static/idic.css';
+</style>
